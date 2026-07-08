51d9e2dd7167308ff8ae2e57ecd4b85421c025b1
Ana-Soares-Vicente <a.vicente@catolicasc.edu.br>
2026-07-08 14:56:04 -0300

    build: setup do projeto backend com Maven, Spring Boot 4.1 e H2

---
+++ battleship-api/src/main/java/com/ana/battleship/BattleshipApplication.java
@@ -0,0 +1,11 @@
+package com.ana.battleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipApplication.class, args);
+	}
+}
